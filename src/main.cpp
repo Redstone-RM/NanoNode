108799a1539a0179e2e3961f69d04b3aa26b66c6
username <username@laptop3>
2022-04-03 10:14:34 -0600

    Working wifi node with teleop_twist_keyboard

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,27 @@
 #include <rcl/error_handling.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
-
 #include <std_msgs/msg/int32.h>
+
+#include <geometry_msgs/msg/twist.h>
+
 
 #if !defined(ESP32) && !defined(TARGET_PORTENTA_H7_M7) && !defined(ARDUINO_NANO_RP2040_CONNECT)
 #error This example is only avaible for Arduino Portenta, Arduino Nano RP2040 Connect and ESP32 Dev module
 #endif
 
-rcl_subscription_t subscriber;
-rclc_executor_t executor;
 
-rcl_publisher_t publisher;
+
 std_msgs__msg__Int32 msg;
-rclc_support_t support; // Support Object
-rcl_allocator_t allocator; //Allocator Object 
-rcl_node_t node;
+rcl_subscription_t subscriber;  // Subscriber  
+geometry_msgs__msg__Twist twistmsg;  // Twist Msg 
+rcl_publisher_t publisher;      // Publisher
+rclc_support_t support;         // Support 
+rcl_allocator_t allocator;      //Allocator
+rclc_executor_t executor;       // Executor
+rcl_node_t node;                // Node 
+
+
 
 #define LED_PIN 13
 
@@ -35,24 +41,19 @@ void error_loop(){
   }
 }
 
-void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
-{
-  RCLC_UNUSED(last_call_time);
-  if (timer != NULL) {
-    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-    msg.data++;
-  }
-}
 
-void subscription_callback(const void * msgin)
-{  
-  const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
-  digitalWrite(LED_PIN, (msg->data == 0) ? LOW : HIGH);  
+//twist message callback
+void subscription_callback(const void *msgin) {
+  const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msgin;
+  // if velocity in x direction is 0 turn off LED, if 1 turn on LED
+  digitalWrite(LED_PIN, (msg->linear.x == 0) ? LOW : HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, HIGH);
 }
 
 void setup() {
   // set_microros_transports();
-  set_microros_wifi_transports(SSID, WIFIPASSWD, AGENTIP, 8888); // defined in the wifi.h
+  set_microros_wifi_transports(SSID, WIFIPASSWD, AGENTIP, 8888); // SSID WIFIPASSWD AGENTIP. All defined in wifi.h. Transport protocol of this function defaults to UDP4
 
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, HIGH);
@@ -62,39 +63,40 @@ void setup() {
   // Initialize micro-ROS allocator
   allocator = rcl_get_default_allocator();
 
-// Initialized support object 
-//create init_options
+  //create init_options
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
   // create node
   RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_wifi_node", "", &support));
+
+  // create subscriber
+  RCCHECK(rclc_subscription_init_default(
+    &subscriber,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+    "micro_ros_arduino_twist_subscriber"));
 
   // create publisher
   RCCHECK(rclc_publisher_init_best_effort(
     &publisher,
     &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "topic_name"));  
+    "bot_status"));  
     msg.data = 0;
 
-  // create subscriber
-  RCCHECK(rclc_subscription_init_default(
-    &subscriber,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "micro_ros_arduino_subscriber")) ;
 
   // create executor
   RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
   RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
 
+
 }
 
 void loop() {
-  RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-  msg.data++;
-  
+ msg.data++; // Useless Example. increment the msg.data 
+ RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL)); // publish data.
+
   delay(100);
-  RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+  RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100))); // Run Executor.
 
 }
